e08a0e0f31b87907819ecf64e08bfa00a882ae6e
JohnPMerrill <john.merrill95@gmail.com>
2016-12-08 11:55:58 -0500

    loading thumbnail from ItemInfo

--- app/src/main/java/edu/uml/cs/jmerrill/fridge_friend/ItemInfo.java
+++ app/src/main/java/edu/uml/cs/jmerrill/fridge_friend/ItemInfo.java
@@ -2,11 +2,16 @@ package edu.uml.cs.jmerrill.fridge_friend;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +45,12 @@ public class ItemInfo extends AppCompatActivity {
         TextView tvUpcCode = (TextView) findViewById(R.id.tv_item_info_upc_code);
         tvUpcCode.setText(upcItem.getId());
 
+        ImageView imgThumbnail = (ImageView) findViewById(R.id.img_item_info_thumbnail);
+        Bitmap thumbnail =
+                BitmapFactory.decodeByteArray(upcItem.getThumbnail(), 0, upcItem.getThumbnail().length);
+
+        imgThumbnail.setImageBitmap(thumbnail);
+        
         Button btnRemoveItem = (Button) findViewById(R.id.btn_item_info_remove_item);
         btnRemoveItem.setOnClickListener(new View.OnClickListener() {
             @Override
